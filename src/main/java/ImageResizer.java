c8d980dc5b3762fe4abda5a2dc6a232fa4426bfd
Mikhail <mcmishabest@gmail.com>
2021-08-06 00:23:36 +0600

    full application

---
+++ src/main/java/ImageResizer.java
@@ -0,0 +1,40 @@
+import org.imgscalr.Scalr;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+public class ImageResizer implements Runnable {
+
+    private File[] files;
+    private int newWidth;
+    private String dstFolder;
+
+    public ImageResizer(File[] files, int newWidth, String dstFolder) {
+        this.files = files;
+        this.newWidth = newWidth;
+        this.dstFolder = dstFolder;
+    }
+
+    @Override
+    public void run() {
+        try {
+            for (File file : files) {
+                BufferedImage image = ImageIO.read(file);
+                if (image == null) {
+                    continue;
+                }
+
+                int newHeight = (int) Math.round(
+                        image.getHeight() / (image.getWidth() / (double) newWidth)
+                );
+
+                BufferedImage newImage = Scalr.resize(image, Scalr.Method.ULTRA_QUALITY, Scalr.Mode.FIT_EXACT, newWidth, newHeight);
+
+                File newFile = new File(dstFolder + "/" + file.getName());
+                ImageIO.write(newImage, "jpg", newFile);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,27 @@
+import java.io.File;
+
+public class Main {
+    private static int newWidth = 300;
+
+    public static void main(String[] args) {
+        String srcFolder = "C:/Users/M/Desktop/src";
+        String dstFolder = "C:/Users/M/Desktop/dst";
+
+        File srcDir = new File(srcFolder);
+
+        File[] files = srcDir.listFiles();
+
+        int middle = files.length / 2;
+
+        File[] files1 = new File[middle];
+        System.arraycopy(files, 0, files1, 0, files1.length);
+        ImageResizer resizer1 = new ImageResizer(files1, newWidth, dstFolder);
+        new Thread(resizer1).start();
+
+        File[] files2 = new File[files.length - middle];
+        System.arraycopy(files, middle, files2, 0, files2.length);
+        ImageResizer resizer2 = new ImageResizer(files2, newWidth, dstFolder);
+        new Thread(resizer2).start();
+
+    }
+}
